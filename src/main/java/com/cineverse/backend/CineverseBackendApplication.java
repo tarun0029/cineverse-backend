89426aaf9de81be86e6115e05bd6d9172fa9e9e5
tarun0029 <tarunchandravanshiji@gmail.com>
2026-07-19 11:44:19 +0530

    initialize CineVerse with Spring Boot, dockerized PostgreSQL, and externalized config

---
+++ src/main/java/com/cineverse/backend/CineverseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cineverse.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineverseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineverseBackendApplication.class, args);
+	}
+
+}
